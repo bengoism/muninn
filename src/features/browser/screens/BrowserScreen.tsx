14953385e125a4a349747ed97f27dba869e4a7f7
axel bengtsson <bengtsson.axel@gmail.com>
2026-04-04 22:40:07 +0200

    fix: BottomSheet as direct child of root flex:1 View, no overlay wrapper

--- src/features/browser/screens/BrowserScreen.tsx
+++ src/features/browser/screens/BrowserScreen.tsx
@@ -663,15 +663,13 @@ export function BrowserScreen() {
         )}
       </SafeAreaView>
 
-      <View style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, zIndex: 10 }} pointerEvents="box-none">
-        <BottomPanel
-          onStart={(g) => agentLoop.start(g)}
-          onCancel={agentLoop.cancel}
-          isRunning={agentLoop.isRunning}
-          modelReady={hasDownloadedModel}
-          modelName={activeModel?.displayName ?? null}
-        />
-      </View>
+      <BottomPanel
+        onStart={(g) => agentLoop.start(g)}
+        onCancel={agentLoop.cancel}
+        isRunning={agentLoop.isRunning}
+        modelReady={hasDownloadedModel}
+        modelName={activeModel?.displayName ?? null}
+      />
     </View>
   );
 }
